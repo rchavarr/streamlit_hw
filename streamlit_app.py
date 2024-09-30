6d354e58b90366d07be92774681f18129872d466
rchavarr <rosalba_barojas@icloud.com>
2024-09-30 11:38:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,6 @@ if selected_sub_categories:
     # Display a line chart for the filtered sales data
     st.line_chart(sales_by_month_filtered, y="Sales")
 
-
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 st.write("### (3) show a line chart of sales for the selected items in (2)")
